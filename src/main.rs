91403700fed2d526407e263c1ef186aeb508b12a
Pablo Pérez <pablo@pabl.eu>
2023-02-28 16:18:48 +0100

    Make counter the frame counter, change how we handle the framerate, change the framerate.

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ pub mod screen;
 // CONSTANTS
 pub const SCREEN_LINES: usize = 32;
 pub const SCREEN_COLUMNS: usize = 64;
-pub const TIMER_HZ: u64 = 60;
+pub const TIMER_HZ: u64 = 500;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -38,29 +38,33 @@ fn main() {
   chip8.load_file(&binary_path);
   screen.setup();
 
-  let mut counter = Duration::new(0, 0); 
+  let mut counter = 0;
   loop {
+    eprint!("\x1b[{};{}H[main] {}", 25, 0, counter);
     let timer = Instant::now();
 
     // Loop until the terminal screen is 32x64.
     screen.require_screen_size(SCREEN_LINES, SCREEN_COLUMNS);
 
-    if screen.update_keys() {
-      break;
     }
 
     let previous_display = chip8.get_display().clone();
-    chip8.fde_loop(screen.pressed_keys);
+    chip8.fde_loop(&screen.pressed_keys);
     
     screen.write(&previous_display, chip8.get_display());
-    
-    counter += timer.elapsed();
-    
-    if counter >= Duration::from_millis(1000/TIMER_HZ) {
-      counter -= Duration::from_millis(1000/TIMER_HZ);
-      if chip8.update_timers() {
-        screen.beep();
-      }
+   
+    let elapsed: Duration = timer.elapsed();
+    // Beep if we can't keep up!
+    if elapsed >= Duration::from_millis((1000/TIMER_HZ)*2) {
+      screen.beep();
     }
+    // If we're too fast, sleep for the remaining time.
+    if elapsed < Duration::from_millis(1000 / TIMER_HZ) {
+      thread::sleep(Duration::from_millis(1000 / TIMER_HZ) - elapsed);
+    }
+    if chip8.update_timers() {
+      screen.beep();
+    }
+    counter += 1;
   }
 }
